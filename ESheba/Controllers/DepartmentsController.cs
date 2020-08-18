7edc967bc2cca2cbd01a7a1db4825b1e2f57a409
kazialauddin <kazi.alauddin421@gmail.com>
2020-08-18 14:53:38 +0600

    Git from new

--- ESheba/Controllers/DepartmentsController.cs
+++ ESheba/Controllers/DepartmentsController.cs
@@ -77,9 +77,7 @@ namespace ESheba.Controllers
             return View(department);
         }
 
-        // POST: Departments/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "DepartmentId,Name")] Department department)
